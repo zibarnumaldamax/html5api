4b19d3ab544bb69f76468c9416cf3e63c38d32a7
zibarnumaldamax <zibarnumaldamax@gmail.com>
2024-01-15 16:13:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function init() {
     var geolocation = ymaps.geolocation,
         myMap = new ymaps.Map('map', {
             center: [55.76, 37.64],
-            zoom: 10,
-            controls: ['routeButtonControl']
+            zoom: 10
         }, {
             searchControlProvider: 'yandex#search'
         });
@@ -21,6 +20,21 @@ function init() {
         });
         myMap.geoObjects.add(result.geoObjects);
     });
+
+    var routePanel = new ymaps.control.RoutePanel({
+        options: {
+            provider: 'yandex#search'
+        }
+    });
+
+    var searchControl = new ymaps.control.SearchControl({
+        options: {
+            provider: 'yandex#search'
+        }
+    });
+    
+    myMap.controls.add(searchControl);
+    myMap.controls.add(routePanel);
 
     var control = myMap.controls.get('routeButtonControl');
     control.routePanel.geolocate('from');
